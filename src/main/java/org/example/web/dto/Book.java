96dd241a07bcae719cad102e5ba5c7efcd1cb796
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-12-08 10:22:18 +0300

    Add "lombok" dependency to pom.xml

--- src/main/java/org/example/web/dto/Book.java
+++ src/main/java/org/example/web/dto/Book.java
@@ -1,42 +1,15 @@
 package org.example.web.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class Book {
+
     private Integer id;
     private String author;
     private String title;
     private Integer size;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Integer getSize() {
-        return size;
-    }
-
-    public void setSize(Integer size) {
-        this.size = size;
-    }
 
     @Override
     public String toString() {
--- src/main/java/org/example/web/dto/LoginForm.java
+++ src/main/java/org/example/web/dto/LoginForm.java
@@ -1,33 +1,17 @@
 package org.example.web.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginForm {
 
     private String username;
     private String password;
-
-    public LoginForm(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-
-    public LoginForm() {
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 
     @Override
     public String toString() {
