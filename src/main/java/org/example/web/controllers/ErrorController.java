022312f95beff64f991376b08ceab10ad96e6079
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-12-17 09:25:37 +0300

    Refactor 404 error handling

--- src/main/java/org/example/web/controllers/ErrorController.java
+++ src/main/java/org/example/web/controllers/ErrorController.java
@@ -1,12 +1,9 @@
 package org.example.web.controllers;
 
-import org.example.app.exceptions.BookShelfLoginException;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@ControllerAdvice
+@Controller
 public class ErrorController {
 
     @GetMapping("/404")
@@ -14,9 +11,4 @@ public class ErrorController {
         return "errors/404";
     }
 
-    @ExceptionHandler(BookShelfLoginException.class)
-    public String handleError(Model model, BookShelfLoginException exception){
-        model.addAttribute("errorMessage", exception.getMessage());
-        return "errors/404";
-    }
 }
