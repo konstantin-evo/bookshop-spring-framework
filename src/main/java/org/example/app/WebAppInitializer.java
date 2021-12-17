efb6dcd2211cd6d571c2f1643c4b4c6796c7b166
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-12-17 16:12:34 +0300

    Register H2 servlet in WebAppInitializer

--- src/main/java/org/example/app/WebAppInitializer.java
+++ src/main/java/org/example/app/WebAppInitializer.java
@@ -3,6 +3,7 @@ package org.example.app;
 import org.apache.log4j.Logger;
 import org.example.app.config.AppContextConfig;
 import org.example.web.config.WebContextConfig;
+import org.h2.server.web.WebServlet;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -37,5 +38,12 @@ public class WebAppInitializer implements WebApplicationInitializer {
         dispatcher.addMapping("/");
 
         logger.info("Dispatcher Servlet is ready");
+
+        ServletRegistration.Dynamic servlet = servletContext.addServlet("h2-console", new WebServlet());
+        servlet.setLoadOnStartup(2);
+        servlet.addMapping("/console/*");
+
+        logger.info("WebServlet H2 is ready");
+
     }
 }
